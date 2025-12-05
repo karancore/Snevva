573fe82ee4e69d7ecf9ed06190fda0e510139279
Ribhav <ribhav@coretegra.com>
2025-12-05 12:18:25 +0530

    app_locaizations files

---
+++ lib/l10n/app_localizations_hi.dart
@@ -0,0 +1,193 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Hindi (`hi`).
+class AppLocalizationsHi extends AppLocalizations {
+  AppLocalizationsHi([String locale = 'hi']) : super(locale);
+
+  @override
+  String get hello => 'नमस्ते';
+
+  @override
+  String get welcome => 'आपके ऐप में आपका स्वागत है!';
+
+  @override
+  String get one => 'Sign In and Sign Up Text';
+
+  @override
+  String get chooseLanguageLabel => 'अपनी परिचित भाषा चुनें';
+
+  @override
+  String get confirmLanguageButton => 'भाषा की पुष्टि करें';
+
+  @override
+  String get alreadyHaveAccount => 'पहले से खाता मौजूद है';
+
+  @override
+  String get loginInText => 'यहाँ लॉगिन करें';
+
+  @override
+  String get nextText => 'आगे बढ़ें';
+
+  @override
+  String get googleText => 'गूगल से साइन अप करें';
+
+  @override
+  String get appleText => 'एप्पल से साइन अप करें';
+
+  @override
+  String get googleTextSignIn => 'गूगल से साइन इन करें';
+
+  @override
+  String get appleTextSignIn => 'एप्पल';
+
+  @override
+  String get fbTextSignIn => 'फेसबुक';
+
+  @override
+  String get socialSignInSingIn => 'सोशल मीडिया से साइन इन करें';
+
+  @override
+  String get socialSignIn => 'सोशल मीडिया से साइन इन करें';
+
+  @override
+  String get createAccount => 'खाता बनाएं';
+
+  @override
+  String get enterEmailOrPhone => 'अपना ईमेल या फोन नंबर दर्ज करें';
+
+  @override
+  String get signInButtonText => 'साइन इन करें';
+
+  @override
+  String get notMemberText => 'सदस्य नहीं हैं?';
+
+  @override
+  String get createNewAccountText => 'नया खाता बनाएं';
+
+  @override
+  String get checkboxRememberMe => 'मुझे याद रखें';
+
+  @override
+  String get linkForgotPassword => 'पासवर्ड भूल गए';
+
+  @override
+  String get inputEmailOrMobile => 'ईमेल / मोबाइल नंबर';
+
+  @override
+  String get inputPassword => 'पासवर्ड';
+
+  @override
+  String get two => 'Forgot Password And Otp Screen';
+
+  @override
+  String get forgetPasswordScreenText =>
+      'कृपया एक सत्यापन कोड प्राप्त करने के लिए\nअपना ईमेल या फोन नंबर दर्ज करें।';
+
+  @override
+  String get sendCode => 'कोड भेजें';
+
+  @override
+  String get verify => 'सत्यापित करें';
+
+  @override
+  String get resendCode => 'कोड फिर से भेजें';
+
+  @override
+  String get enter6DigitCodeText => 'कृपया भेजा गया 6 अंकों का कोड दर्ज करें';
+
+  @override
+  String get verifyPhoneNumber => 'अपना फोन नंबर सत्यापित करें';
+
+  @override
+  String get verifyEmailAddress => 'अपना ईमेल पता सत्यापित करें';
+
+  @override
+  String get three => 'New Password And Update Screen';
+
+  @override
+  String get agreeToTerms => 'मैं सहमत हूँ, ';
+
+  @override
+  String get agreeToConditions => 'नियम और शर्तों';
+
+  @override
+  String get passwordsMatch => 'पासवर्ड मेल खाते हैं';
+
+  @override
+  String get passwordsDoNotMatch => 'पासवर्ड मेल नहीं खाते';
+
+  @override
+  String get passwordMinLength => '10 अक्षर';
+
+  @override
+  String get passwordSpecialChar => '1 संख्या या विशेष अक्षर (उदाहरण: # ? ! &)';
+
+  @override
+  String get passwordLetterRequirement => '1 अक्षर';
+
+  @override
+  String get passwordMustContain =>
+      'आपका पासवर्ड में कम से कम ये शामिल होने चाहिए';
+
+  @override
+  String get confirmPassword => 'अपना पासवर्ड की पुष्टि करें';
+
+  @override
+  String get passwordStrengthStrong => 'पासवर्ड मजबूत है';
+
+  @override
+  String get passwordStrengthWeak => 'कमजोर पासवर्ड';
+
+  @override
+  String get newPasswordInstruction =>
+      'अपना नया पासवर्ड दर्ज करें और सुनिश्चित करें कि यह पिछले से अलग हो';
+
+  @override
+  String get updatePassword => 'पासवर्ड अपडेट करें';
+
+  @override
+  String get createPassword => 'पासवर्ड बनाएं';
+
+  @override
+  String get newPassword => 'नया पासवर्ड';
+
+  @override
+  String get update => 'अपडेट करें';
+
+  @override
+  String get enterPassword => 'अपना पासवर्ड दर्ज करें';
+
+  @override
+  String get four => 'Profile Setup And Questionnaire Screen';
+
+  @override
+  String get setupProfile => 'चलिए आपकी प्रोफ़ाइल सेटअप करते हैं 😊';
+
+  @override
+  String get pleaseEnterYourName => 'कृपया अपना नाम दर्ज करें';
+
+  @override
+  String get selectGender => 'लिंग चुनें';
+
+  @override
+  String get birthdayPrompt => 'आपका जन्मदिन कब है?';
+
+  @override
+  String get submit => 'सेव करें';
+
+  @override
+  String get other => 'अन्य';
+
+  @override
+  String get preferNotToSay => 'कहना नहीं चाहता';
+
+  @override
+  String get male => 'पुरुष';
+
+  @override
+  String get female => 'महिला';
+}
