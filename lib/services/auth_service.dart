c527de57402e6f834d1191bbad858d1c833a1706
karancore <karancoretegra@gmail.com>
2026-04-09 12:14:18 +0530

    chore(auth): keep step refactor out of logout cleanup

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:alarm/alarm.dart';
-import 'package:hive/hive.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:snevva/Controllers/DietPlan/diet_plan_controller.dart';
@@ -25,7 +24,6 @@ import 'package:snevva/common/custom_snackbar.dart';
 import 'package:snevva/common/global_variables.dart';
 import 'package:snevva/consts/consts.dart';
 import 'package:snevva/env/env.dart';
-import 'package:snevva/models/hive_models/steps_model.dart';
 import 'package:snevva/services/api_service.dart';
 import 'package:snevva/views/SignUp/sign_in_screen.dart';
 import 'package:snevva/views/permissions/permission_gate_screen.dart';
@@ -371,17 +369,6 @@ class AuthService {
 
       final prefs = await SharedPreferences.getInstance();
       await prefs.clear();
-
-      try {
-        await Hive.box<StepEntry>('step_history').clear();
-      } catch (e) {
-        debugPrint('❌ Failed to clear step_history on logout: $e');
-        try {
-          await Hive.box<StepEntry>('step_history').clear();
-        } catch (e2) {
-          debugPrint('❌ Second attempt to clear step_history failed: $e2');
-        }
-      }
 
       final localStorageManager = Get.find<LocalStorageManager>();
       localStorageManager.userMap.value = {};
